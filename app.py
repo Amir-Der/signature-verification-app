4732f0449c4ad8468e6864d380df7af114b4fe91
Amir-Der <derakhshan.amir.m@gmail.com>
2025-04-29 21:18:51 +0330

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def index():
             features = scaler.transform(features)
             features = pca.transform(features)
             pred = model.predict(features)[0]
-            result = "امضا معتبر است ✅" if pred == 0 else "امضا جعلی است ❌"
+            result = "امضا معتبر است ✅" if pred == 1 else "امضا جعلی است ❌"
         except Exception as e:
             result = f"خطا در پردازش تصویر: {e}"
 
